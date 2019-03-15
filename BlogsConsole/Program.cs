90ad53625832b6bf46e57e91736ca46da91214d7
Cirus Sckackel <cschackel@my.wctc.edu>
2019-03-15 09:58:39 -0500

    Added Basic Project

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NLog;
+using BlogsConsole.Models;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlogsConsole
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public static void Main(string[] args)
         {
+            logger.Info("Program started");
+            try
+            {
+
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+
+                var db = new BloggingContext();
+                db.AddBlog(blog);
+                logger.Info("Blog added - {name}", name);
+
+                // Display all Blogs from the database
+                var query = db.Blogs.OrderBy(b => b.Name);
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+            Console.WriteLine("");
+            logger.Info("Program ended");
         }
+        
     }
 }
